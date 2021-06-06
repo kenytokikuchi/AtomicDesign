6c2ffa60c94b4f02c37ca8da2a5a80ec64d2374f
kento <35658197+kenytokikuchi@users.noreply.github.com>
2021-06-06 23:16:54 +0900

    cordを挿入

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "kento",
+  image: "https://source.unsplash.com/Sg3XwuEpybU",
+  email: "qwfdfafaf@example.com",
+  phone: "090-8888-7777",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>テスト</PrimaryButton>
+      <PrimaryButton>検索</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
