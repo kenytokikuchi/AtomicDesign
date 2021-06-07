f9a15dda683772aec0e4c7ada84ceadfdcf429e5
kento <35658197+kenytokikuchi@users.noreply.github.com>
2021-06-08 00:54:36 +0900

    atmicdesign完成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "kento",
-  image: "https://source.unsplash.com/Sg3XwuEpybU",
-  email: "qwfdfafaf@example.com",
-  phone: "090-8888-7777",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <PrimaryButton>検索</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
+    <>
+      <Router />
+    </>
   );
 }
